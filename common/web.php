f577bcc05fdf1cbdb2f4fee09cf37c4fce726151
Dmitriy Derepko <xepozz@list.ru>
2020-10-05 23:01:59 +0300

    Add base configuration

---
+++ common/web.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Yiisoft\Session\Session;
+use Yiisoft\Session\SessionInterface;
+
+/* @var $params array */
+
+return [
+    SessionInterface::class => fn() => new Session(
+        $params['yiisoft/session']['session']['options'],
+        $params['yiisoft/session']['session']['handler']
+    ),
+];
