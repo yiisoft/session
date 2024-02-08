9d88197ddd1aa64ef150cfac63da657637dee047
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-02-08 21:44:49 +0300

    Fix psalm (#55)

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\FunctionLike\MixedTypeRector;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -18,5 +19,9 @@ return static function (RectorConfig $rectorConfig): void {
     // define sets of rules
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_80,
+    ]);
+
+    $rectorConfig->skip([
+        MixedTypeRector::class,
     ]);
 };
