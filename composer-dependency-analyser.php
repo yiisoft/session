3a18487fa1862e90bbfcc0177e5ad96e4ca8113e
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-08 08:21:25 +0300

    Add Composer Dependency Analyser

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->disableComposerAutoloadPathScan()
+    ->setFileExtensions(['php'])
+    ->addPathToScan(__DIR__ . '/config', isDev: false)
+    ->addPathToScan(__DIR__ . '/src', isDev: false)
+    ->addPathToScan(__DIR__ . '/tests', isDev: true)
+    ->ignoreErrorsOnExtension('ext-session', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackages(['psr/http-message-implementation'], [ErrorType::UNUSED_DEPENDENCY]);
