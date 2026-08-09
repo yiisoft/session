81baa39b723b26e6ab620e69f60577b30e2f1db0
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-09 19:28:55 +0300

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
 use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
-return (new Configuration())
+$config = (new Configuration())
     ->disableComposerAutoloadPathScan()
     ->setFileExtensions(['php'])
     ->addPathToScan(__DIR__ . '/config', isDev: false)
@@ -13,3 +13,12 @@ return (new Configuration())
     ->addPathToScan(__DIR__ . '/tests', isDev: true)
     ->ignoreErrorsOnExtension('ext-session', [ErrorType::SHADOW_DEPENDENCY])
     ->ignoreErrorsOnPackages(['psr/http-message-implementation'], [ErrorType::UNUSED_DEPENDENCY]);
+
+// The `ReturnTypeWillChange` attribute used in tests is native since PHP 8.1 and is polyfilled by
+// symfony/polyfill-php81 (pulled in transitively) on PHP 8.0, so it only shows up as a shadow
+// dependency when running under PHP 8.0.
+if (PHP_VERSION_ID < 80100) {
+    $config->ignoreErrorsOnPackages(['symfony/polyfill-php81'], [ErrorType::SHADOW_DEPENDENCY]);
+}
+
+return $config;
